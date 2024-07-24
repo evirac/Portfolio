73080d964e6d2c0978a94c76d64748941345f73a
Ayush Gupta <ayushguptanew2@gmail.com>
2024-07-24 09:24:10 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-2'>
+        <div className="flex flex-shrink-0 items-center">
+            <img className='mx-2 w-10' src='/assets/kevinRushLogo.png' alt='logo'/>
+        </div>
+        <div  className='m-8 flex flex-shrink-0 items-center gap-4 text-2xl'>
+          <FaLinkedin/>
+          <FaGithub/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
